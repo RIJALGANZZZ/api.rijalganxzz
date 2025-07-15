362a9a095d109fd48e9282fe7f513dc445f99ac4
rijalganzz <keyshan430@gmail.com>
2025-07-15 19:29:15 +0700

    Add files via upload

--- api/imagecreator/brat.js
+++ api/imagecreator/brat.js
@@ -18,14 +18,14 @@ app.get('/imagecreator/bratvideo', async (req, res) => {
         try {
             const { apikey, text } = req.query
             if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
-            const pedo = await getBuffer(`https://aqul-brat.hf.space/?text=${encodeURIComponent(text)}&mode=animated`)
+            const pedo = await getBuffer(`https://fastrestapis.fasturl.cloud/maker/brat/animated?text=${text}&mode=animated`)
             res.writeHead(200, {
                 'Content-Type': 'video/mp4',
-                'Content-Length': pedo.length
-            })
-            res.end(pedo)
+                'Content-Length': pedo.length,
+            });
+            res.end(pedo);
         } catch (error) {
-            res.status(500).send(`Error: ${error.message}`)
+            res.status(500).send(`Error: ${error.message}`);
         }
-    })
+    });    
 }
