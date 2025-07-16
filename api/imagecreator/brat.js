70e9ae747690557292c82d3204770dd9157058b9
rijalganzz <keyshan430@gmail.com>
2025-07-16 17:54:32 +0700

    Update brat.js

--- api/imagecreator/brat.js
+++ api/imagecreator/brat.js
@@ -15,17 +15,21 @@ app.get('/imagecreator/brat', async (req, res) => {
     })
 
 app.get('/imagecreator/bratvideo', async (req, res) => {
-        try {
-            const { apikey, text } = req.query
-            if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
-            const pedo = await getBuffer(`https://zenzxz.dpdns.org/maker/bratvid?text=${text}`)
-            res.writeHead(200, {
-                'Content-Type': 'video/mp4',
-                'Content-Length': pedo.length,
-            });
-            res.end(pedo);
-        } catch (error) {
-            res.status(500).send(`Error: ${error.message}`);
-        }
-    });    
-}
+  try {
+    const { apikey, text } = req.query;
+    if (!text) return res.json({ status: false, error: 'Parameter "text" wajib diisi' });
+    if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' });
+
+    const json = await Func.fetchJson(`https://ytdlpyton.nvlgroup.my.id/maker/bratvid?text=${encodeURIComponent(text)}`);
+    if (!json.video_url) return res.json({ status: false, error: 'Gagal mengambil video' });
+
+    const videoBuffer = await Func.fetchBuffer(json.video_url);
+    res.writeHead(200, {
+      'Content-Type': 'video/mp4',
+      'Content-Length': videoBuffer.length
+    });
+    res.end(videoBuffer);
+  } catch (error) {
+    res.status(500).send(`Error: ${error.message}`);
+  }
+});
